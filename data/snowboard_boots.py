28ba158259b3d4e24b8956e5d45ca6420dd891d1
johnpaultort <johnpaultort@csu.fullerton.edu>
2026-05-21 18:01:00 -0700

    Newer files that seperate everything to be cleaner and shorter.

---
+++ data/snowboard_boots.py
@@ -0,0 +1,62 @@
+import json
+
+Boots = [
+    # Ride Boots
+    {
+        "name": "Lasso",
+        "brand": "Ride",
+        "style"; ["all-mountain"],
+        "flex": "7",
+        "size": [7, 7.5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 12, 13, 14, 15],
+        "lacing": ["double-boa"],
+        "price"; "379.95",
+        "image": "https://www.sportsbasement.com/cdn/shop/files/100303347_BLK_1.png?crop=center&height=800&v=1778544478&width=800",
+        "notes": "."
+    },
+    {
+        "name": "Anthem",
+        "brand": "Ride",
+        "style": ["all-mmountain"],
+        "flex": "3",
+        "size": [7, 7.5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 12, 13, 14, 15],
+        "lacing": ["single-boa"],
+        "price": "289.95",
+        "image": "https://www.sportsbasement.com/cdn/shop/files/100303348_BLK_1.png?crop=center&height=800&v=1778545677&width=800",
+        "notes": ""
+    },
+    {
+        "name": "Lasso Pro",
+        "brand": "Ride",
+        "style": ["all-mmountain", "freeride"],
+        "flex": "8",
+        "size": [7, 7.5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 12, 13, 14, 15],
+        "lacing": ["double-boa"],
+        "price": "289.95",
+        "image": "",
+        "notes": ""
+    },
+    {
+        "name": "Deadbolt Zonal",
+        "brand": "Ride",
+        "style": ["all-mmountain"],
+        "flex": "7",
+        "size": [7, 7.5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 12, 13, 14, 15],
+        "lacing": ["double-boa"],
+        "price": "289.95",
+        "image": "",
+        "notes": ""
+    },
+    {
+        "name": "Insano",
+        "brand": "Ride",
+        "style": ["all-mmountain"],
+        "flex": "10",
+        "size": [7, 7.5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 12, 13, 14, 15],
+        "lacing": ["double-boa"],
+        "price": "289.95",
+        "image": "",
+        "notes": ""
+    },
+
+
+]
